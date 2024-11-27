897a7b84b880129bb4cae675c9679b84a1a4819d
kevinart-maker <kevinodeyemi76@gmail.com>
2024-11-27 04:25:51 +0100

    cleaning up..

--- src/App.jsx
+++ src/App.jsx
@@ -45,31 +45,18 @@ function App() {
           <Loading />
         ) : (
               <div>
-            {/* <Routes>
-              <Route index element={<Preview />}/>
-              <Route path='login' element={<Login />}/>
-              <Route path='signup' element={<Create />}/>
-              <Route path='dashboard' element={<AuthStatus />}/>
-              <Route path='language' element={<Language />}/>
-              <Route path='createprofile' element={<CreateProfile />}/>
-              <Route path='profile' element={<Profile />}/>
-              <Route path='complaint' element={<ComplaintForm />}/>
-              <Route path='donate' element={<Donate />}/>
-              <Route path='donatesuccess' element={<DonateSuccess />}/>
-            </Routes> */}
-
-            <Routes>
-              <Route path='preview' element={!user ? <Preview /> : <Navigate to="/"/>}/>
-              <Route path="login" element={!user ? <Login /> : <Navigate to="/" />}/>
-              <Route path="signup" element={user ? <Create /> : <Navigate to="/preview" />}/>
-              <Route path="language" element={!user ? <Language /> : <Navigate to="/" />}/>
-              <Route path='createprofile' element={user ? <CreateProfile /> : <Navigate to="/preview"/>}/>
-              <Route index element={user ? <Profile /> : <Navigate to="/preview"/>} />
-              <Route path="complaint" element={user ? <ComplaintForm /> : <Navigate to="/preview"/>} />
-              <Route path="donate" element={user ? <Donate /> : <Navigate to="/login"/>} />
-              <Route path="donatesuccess" element={user ? <DonateSuccess /> : <Navigate to="/preview"/>} />
-              <Route path='*' element={<NotFound />}/>
-          </Routes>
+                <Routes>
+                  <Route path='preview' element={!user ? <Preview /> : <Navigate to="/"/>}/>
+                  <Route path="login" element={!user ? <Login /> : <Navigate to="/" />}/>
+                  <Route path="signup" element={user ? <Create /> : <Navigate to="/preview" />}/>
+                  <Route path="language" element={!user ? <Language /> : <Navigate to="/" />}/>
+                  <Route path='createprofile' element={user ? <CreateProfile /> : <Navigate to="/preview"/>}/>
+                  <Route index element={user ? <Profile /> : <Navigate to="/preview"/>} />
+                  <Route path="complaint" element={user ? <ComplaintForm /> : <Navigate to="/preview"/>} />
+                  <Route path="donate" element={user ? <Donate /> : <Navigate to="/login"/>} />
+                  <Route path="donatesuccess" element={user ? <DonateSuccess /> : <Navigate to="/preview"/>} />
+                  <Route path='*' element={<NotFound />}/>
+              </Routes>
           </div>
         )
       }
