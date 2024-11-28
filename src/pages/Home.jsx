60a32c65848925d841b7c56bfe88d3b44c1f55d9
kevinart-maker <kevinodeyemi76@gmail.com>
2024-11-28 12:37:04 +0100

    updated maps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
             <div className="map">
                 <h2>Nearest Police Station</h2>
                 <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31799.256451700963!2d8.313279663560206!3d4.955113985812299!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1067863603a882e1%3A0xb1c9e9b57b719d6e!2sBogoberi%2C%20Calabar%20540281%2C%20Cross%20River!5e0!3m2!1sen!2sng!4v1732754352071!5m2!1sen!2sng"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.786755876278!2d8.3288643!3d4.9750746999999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x106787c86faaaaab%3A0x8f2de14e86b1a335!2sCross%20River%20State%20Police%20Command%20Headquarters!5e0!3m2!1sen!2sng!4v1732793698828!5m2!1sen!2sng"
             width="100%"
             height="100%"
             style={{ border: 0 }}
@@ -28,7 +28,7 @@ const Home = () => {
             <div className="map">
                 <h2>Nearest Hospital</h2>
                 <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127185.27129015603!2d8.280525745872213!3d5.015814103788819!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x105d7d855cacd2bb%3A0x69af84a8dca692a!2sCalabar%20Municipal%2C%20Cross%20River!5e0!3m2!1sen!2sng!4v1732749907543!5m2!1sen!2sng"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.9061409132114!2d8.3346616!3d4.9552662!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x106786489e1e9917%3A0x45e83d3f1148e0fa!2sASI%20Ukpo%20Hospitals!5e0!3m2!1sen!2sng!4v1732793762792!5m2!1sen!2sng"
             width="100%"
             height="100%"
             style={{ border: 0 }}
